d602f059a83384f48074e2c27f08347ad060aea5
Joe Goller <joe.goller@aon.com>
2019-05-20 16:54:02 +0000

    Adding scripts for enabling ldap.

---
+++ ci/scripts/enable-ldap.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+[[ ${DEBUG,,} == true ]] && set -x
+set -eu
+
+source pipeline/ci/scripts/common.sh
+
+function configure_ldap() {
+  om -t $OM_TARGET \
+    $om_options \
+    curl \
+      --path /api/v0/setup \
+      -X POST \
+      -H "Content-Type: application/json" \
+      -d '{ "setup": {
+        "identity_provider": "ldap",
+        "decryption_passphrase": "$OM_DECRYPTION_PASSWORD",
+        "decryption_passphrase_confirmation":"$OM_DECRYPTION_PASSWORD",
+        "eula_accepted": "$EULA_ACCEPTED",
+        "ldap_settings": {
+          "server_url": "$LDAP_SERVER_URL",
+          "ldap_username": "$LDAP_USERNAME",
+          "ldap_password": "$LDAP_PASSWORD",
+          "user_search_base": "$LDAP_USER_SEARCH_BASE",
+          "user_search_filter": "$LDAP_SEARCH_FILTER",
+          "group_search_base": "$LDAP_GROUP_SEARCH_BASE",
+          "group_search_filter": "$LDAP_GROUP_SEARCH_FILTER",
+          "ldap_rbac_admin_group_name": "$LDAP_RBAC_ADMIN_GROUP_NAME",
+          "email_attribute": "$LDAP_EMAIL_ATTRIBUTE",
+          "ldap_referrals": "$LDAP_REFERRALS"
+        }
+      } }'
+}
+
+configure_ldap
