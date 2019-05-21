8e44e738ee223bc06a44d2e8245601ca881135aa
Joe Goller <joe.goller@aon.com>
2019-05-21 21:22:00 +0000

    Fixing formatting of vars in data.

--- ci/scripts/enable-ldap.sh
+++ ci/scripts/enable-ldap.sh
@@ -15,20 +15,20 @@ function configure_ldap() {
       --header "Content-Type: application/json" \
       --data '{ "setup": {
         "identity_provider": "ldap",
-        "decryption_passphrase": $OM_DECRYPTION_PASSWORD,
-        "decryption_passphrase_confirmation": $OM_DECRYPTION_PASSWORD,
-        "eula_accepted": $EULA_ACCEPTED,
+        "decryption_passphrase": "'"$OM_DECRYPTION_PASSWORD"'",
+        "decryption_passphrase_confirmation": "'"$OM_DECRYPTION_PASSWORD"'",
+        "eula_accepted": "'"$EULA_ACCEPTED"'",
         "ldap_settings": {
-          "server_url": $LDAP_SERVER_URL,
-          "ldap_username": $LDAP_USERNAME,
-          "ldap_password": $LDAP_PASSWORD,
-          "user_search_base": $LDAP_USER_SEARCH_BASE,
-          "user_search_filter": $LDAP_SEARCH_FILTER,
-          "group_search_base": $LDAP_GROUP_SEARCH_BASE,
-          "group_search_filter": $LDAP_GROUP_SEARCH_FILTER,
-          "ldap_rbac_admin_group_name": $LDAP_RBAC_ADMIN_GROUP_NAME,
-          "email_attribute": $LDAP_EMAIL_ATTRIBUTE,
-          "ldap_referrals": $LDAP_REFERRALS
+          "server_url": "'"$LDAP_SERVER_URL"'",
+          "ldap_username": "'"$LDAP_USERNAME"'",
+          "ldap_password": "'"$LDAP_PASSWORD"'",
+          "user_search_base": "'"$LDAP_USER_SEARCH_BASE"'",
+          "user_search_filter": "'"$LDAP_SEARCH_FILTER"'",
+          "group_search_base": "'"$LDAP_GROUP_SEARCH_BASE"'",
+          "group_search_filter": "'"$LDAP_GROUP_SEARCH_FILTER"'",
+          "ldap_rbac_admin_group_name": "'"$LDAP_RBAC_ADMIN_GROUP_NAME"'",
+          "email_attribute": "'"$LDAP_EMAIL_ATTRIBUTE"'",
+          "ldap_referrals": "'"$LDAP_REFERRALS"'"
         }
       } }'
 }
