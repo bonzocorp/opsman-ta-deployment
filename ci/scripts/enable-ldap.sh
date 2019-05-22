bf7a574b283f788b88b11bcb537a6f9e7b7eafa7
Joe Goller <joe.goller@aon.com>
2019-05-22 04:38:09 +0000

    Adjusting syntax for opsman 2.4 instead of 2.5

--- ci/scripts/enable-ldap.sh
+++ ci/scripts/enable-ldap.sh
@@ -18,30 +18,38 @@ function enable_ldap() {
 }
 
 function configure_ldap() {
+#  om -t $OM_TARGET \
+#    $om_options \
+#    curl \
+#      --path /api/v0/setup \
+#      --request POST \
+#      --header "Content-Type: application/json" \
+#      --data '{ "setup": {
+#        "identity_provider": "ldap",
+#        "decryption_passphrase": "'"$OM_DECRYPTION_PASSWORD"'",
+#        "decryption_passphrase_confirmation": "'"$OM_DECRYPTION_PASSWORD"'",
+#        "eula_accepted": "'"$EULA_ACCEPTED"'",
+#        "ldap_settings": {
+#          "server_url": "'"$LDAP_SERVER_URL"'",
+#          "ldap_username": "'"$LDAP_USERNAME"'",
+#          "ldap_password": "'"$LDAP_PASSWORD"'",
+#          "user_search_base": "'"$LDAP_USER_SEARCH_BASE"'",
+#          "user_search_filter": "'"$LDAP_SEARCH_FILTER"'",
+#          "group_search_base": "'"$LDAP_GROUP_SEARCH_BASE"'",
+#          "group_search_filter": "'"$LDAP_GROUP_SEARCH_FILTER"'",
+#          "ldap_rbac_admin_group_name": "'"$LDAP_RBAC_ADMIN_GROUP_NAME"'",
+#          "email_attribute": "'"$LDAP_EMAIL_ATTRIBUTE"'",
+#          "ldap_referrals": "'"$LDAP_REFERRALS"'"
+#        }
+#      } }'
+
   om -t $OM_TARGET \
     $om_options \
-    curl \
-      --path /api/v0/setup \
-      --request POST \
-      --header "Content-Type: application/json" \
-      --data '{ "setup": {
-        "identity_provider": "ldap",
-        "decryption_passphrase": "'"$OM_DECRYPTION_PASSWORD"'",
-        "decryption_passphrase_confirmation": "'"$OM_DECRYPTION_PASSWORD"'",
-        "eula_accepted": "'"$EULA_ACCEPTED"'",
-        "ldap_settings": {
-          "server_url": "'"$LDAP_SERVER_URL"'",
-          "ldap_username": "'"$LDAP_USERNAME"'",
-          "ldap_password": "'"$LDAP_PASSWORD"'",
-          "user_search_base": "'"$LDAP_USER_SEARCH_BASE"'",
-          "user_search_filter": "'"$LDAP_SEARCH_FILTER"'",
-          "group_search_base": "'"$LDAP_GROUP_SEARCH_BASE"'",
-          "group_search_filter": "'"$LDAP_GROUP_SEARCH_FILTER"'",
-          "ldap_rbac_admin_group_name": "'"$LDAP_RBAC_ADMIN_GROUP_NAME"'",
-          "email_attribute": "'"$LDAP_EMAIL_ATTRIBUTE"'",
-          "ldap_referrals": "'"$LDAP_REFERRALS"'"
-        }
-      } }'
+    curl  \
+    --path /api/v0/settings/rbac \
+    --request PUT \
+    --header "Content-Type: application/json" \
+    --data '{"ldap_rbac_admin_group_name": "'"$LDAP_RBAC_ADMIN_GROUP_NAME"'"}'
 }
 
 load_custom_ca_certs
