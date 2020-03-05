bde1c810ab3d51cb1674d966da228a3e6ff609c0
Alan Moran <alan.moran1@aon.com>
2020-03-05 17:19:56 +0000

    Merge root and intermidiate custom ca certs in a single entry
    Signed-off-by: Adam Roffler <adam.roffler@aon.com>

--- ci/scripts/update-certificates.sh
+++ ci/scripts/update-certificates.sh
@@ -3,29 +3,27 @@
 source pipeline/ci/scripts/common.sh
 
 function update_certificates() {
-  local six_month_certs="$(om curl -s --path /api/v0/deployed/certificates?expires_within=6m)"
-  local three_month_certs="$(om curl -s --path /api/v0/deployed/certificates?expires_within=3m)"
-
-
+  local six_month_certs="$(om curl -s --path /api/v0/deployed/certificates?expires_within=6m | jq '.certificates' -r)"
+  local three_month_certs="$(om curl -s --path /api/v0/deployed/certificates?expires_within=3m | jq '.certificates' -r)"
 
   log "\nNON ROTATABLE CERTIFICATES:"
   log "expiring <6m:"
-  echo $six_month_certs | jq '.certificates | .[] | select(.variable_path=="/opsmgr/bosh_dns/tls_ca") | .property_reference' -r
+  echo $six_month_certs | jq '.[] | select(.variable_path=="/opsmgr/bosh_dns/tls_ca") | .property_reference' -r
   log "expiring <3m:"
-  echo $three_month_certs | jq '.certificates | .[] | select(.variable_path=="/opsmgr/bosh_dns/tls_ca") | .property_reference' -r
+  echo $three_month_certs | jq '.[] | select(.variable_path=="/opsmgr/bosh_dns/tls_ca") | .property_reference' -r
 
 
   log "\nNON CONFIGURABLE CERTIFICATES:"
   log "expiring <6m:"
-  echo $three_month_certs | jq '.certificates | .[] | select(.configurable==false) | select(.location|test("opsman|credhub"))| .property_reference' -r
+  echo $three_month_certs | jq '.[] | select(.configurable==false) | select(.location|test("opsman|credhub"))| .property_reference' -r
   log "expiring <3m:"
-  echo $six_month_certs | jq '.certificates | .[] | select(.configurable==false) | select(.location|test("opsman|credhub"))| .property_reference' -r
+  echo $six_month_certs | jq '.[] | select(.configurable==false) | select(.location|test("opsman|credhub"))| .property_reference' -r
 
   log "\nCONFIGURABLE CERTIFICATE:"
   log "expiring <6m:"
-  echo $six_month_certs | jq '.certificates | .[] | select(.configurable==true) | .property_reference' -r
+  echo $six_month_certs | jq '.[] | select(.configurable==true) | ' -r
   log "expiring <3m:"
-  echo $three_month_certs | jq '.certificates | .[] | select(.configurable==true) | .property_reference' -r
+  echo $three_month_certs | jq '.[] | select(.configurable==true) | .property_reference' -r
 
 }
 
