8b6e962042cec0a6d1857f2b00b6e3d5faea588e
Alan Moran <alan.moran1@aon.com>
2020-03-19 20:47:43 +0000

    Adding debug from configs to update certs.

--- ci/scripts/update-certificates.sh
+++ ci/scripts/update-certificates.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+[[ ${DEBUG,,} == true ]] && set -x
+set -eu
+
 scripts_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source $scripts_dir/common.sh
 
